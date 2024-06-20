fe1f37cfc6ba586a6355a6547eeea2647df6272c
MikhailPakhomov <mihail93@mail.ru>
2024-06-20 18:17:54 +0300

    feat: изменен компонент кнопки Добавить. Добавлены функции увеличения и уменьшения количества пицц сразу в блоке с пиццей

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch, useSelector } from 'react-redux';
-import { addPizza, increment, selectCart } from './../../redux/slices/cartSlice';
+import { addPizza, increment, decrement, selectCart } from './../../redux/slices/cartSlice';
 import { Link } from 'react-router-dom';
 
-export default function PizzaBlock({ id,title, price, imageUrl, sizes, types }) {
+export default function PizzaBlock({ id, title, price, imageUrl, sizes, types }) {
   const [activeSize, setActiveSize] = React.useState(0);
   const [activeTypePizza, setActiveTypePizza] = React.useState(0);
   const typePizza = ['тонкое', 'традиционное'];
@@ -31,7 +31,20 @@ export default function PizzaBlock({ id,title, price, imageUrl, sizes, types }) 
     setActiveTypePizza(index);
   };
 
-  const handleAddToCart = (arr) => {
+  const handleAddToCart = (arr) => {};
+
+  const getQtyAddedPizza = (arr) => {
+    const result = arr.find(
+      (item) =>
+        item.title === title &&
+        item.size === sizes[activeSize] &&
+        item.dough === typePizza[activeTypePizza],
+    );
+    if (!result) return 0;
+    return result.count;
+  };
+
+  const handlePlus = () => {
     const index = cart.findIndex(
       (item) =>
         item.title === title &&
@@ -48,16 +61,14 @@ export default function PizzaBlock({ id,title, price, imageUrl, sizes, types }) 
       return;
     }
   };
-
-  const getQtyAddedPizza = (arr) => {
-    const result = arr.find(
+  const handleMinus = () => {
+    const index = cart.findIndex(
       (item) =>
         item.title === title &&
         item.size === sizes[activeSize] &&
         item.dough === typePizza[activeTypePizza],
     );
-    if (!result) return 0;
-    return result.count;
+    dispatch(decrement(index));
   };
   const sizesList = sizes.map((size, index) => {
     return (
@@ -96,21 +107,66 @@ export default function PizzaBlock({ id,title, price, imageUrl, sizes, types }) 
         </div>
         <div className="pizza-block__bottom">
           <div className="pizza-block__price"> {price} ₽</div>
-          <button className="button button--outline button--add" onClick={handleAddToCart}>
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-                fill="white"
-              />
-            </svg>
-            <span>Добавить</span>
-            <i>{getQtyAddedPizza(cart)}</i>
-          </button>
+          {getQtyAddedPizza(cart) === 0 ? (
+            <button className="button button--outline button--add" onClick={handlePlus}>
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 12 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
+                  fill="white"
+                />
+              </svg>
+              <span>Добавить</span>
+            </button>
+          ) : (
+            <div className="button button--after-added">
+              <div className="cart__item-count">
+                <div
+                  className="button button--outline button--circle cart__item-count-minus"
+                  onClick={handleMinus}>
+                  <svg
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                      fill="#EB5A1E"
+                    />
+                    <path
+                      d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                      fill="#EB5A1E"
+                    />
+                  </svg>
+                </div>
+                <b>{getQtyAddedPizza(cart)}</b>
+                <button
+                  className="button button--outline button--circle cart__item-count-plus"
+                  onClick={handlePlus}>
+                  <svg
+                    width="10"
+                    height="10"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                      fill="#EB5A1E"
+                    />
+                    <path
+                      d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                      fill="#EB5A1E"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
